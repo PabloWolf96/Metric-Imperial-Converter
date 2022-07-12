abb34be7eaafcdc8fc5b5a13dbde60797b61138d
Pablowolf96 <ikhena.owen47@gmail.com>
2022-07-12 23:41:51 +0100

    test getNum method

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,67 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
+  this.getNum = function (input) {
+    if (/^[A-Za-z]+$/.test(input)) {
+      return 1;
+    }
+    // regex for matching whole numbers, decimal numbers and fractions
+    let result = input.match(
+      /^\d+(\.\d+|\/[1-9][0-9]*|\.\d+\/[1-9][0-9]*|\/\d+\.\d+|\.\d+\/\d+\.\d+)?\w+$/g
+    );
+
+    if (!result) {
+      return "invalid input";
+    }
+    let [numerator, denomenator] = result[0].split("/");
+    if (!denomenator) {
+      return parseFloat(numerator);
+    }
+    let division = parseFloat(numerator) / parseFloat(denomenator);
+    if (division !== Infinity) {
+      return division;
+    }
+    return "invalid input";
   };
-  
-  this.getUnit = function(input) {
+
+  this.getUnit = function (input) {
     let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
+  this.getReturnUnit = function (initUnit) {
     let result;
-    
+
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.spellOutUnit = function (unit) {
+    switch (unit.toLowerCase()) {
+      case "gal":
+        return "gallons";
+      case "lbs":
+        return "pounds";
+      case "kg":
+        return "kilograms";
+      case "mi":
+        return "miles";
+    }
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
+
     let result;
-    
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+
     return result;
   };
-  
 }
 
 module.exports = ConvertHandler;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,52 @@
-const chai = require('chai');
+const chai = require("chai");
 let assert = chai.assert;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
-
+suite("Unit Tests", function () {
+  suite("convertHandler Function getNum", function () {
+    test("convertHandler should correctly read a whole number input.", () => {
+      assert.strictEqual(
+        convertHandler.getNum("100lbs"),
+        100,
+        'convertHandler.getNum("100lbs") should return 100'
+      );
+    });
+    test("convertHandler should correctly read a decimal number input", () => {
+      assert.strictEqual(
+        convertHandler.getNum("123.5lbs"),
+        123.5,
+        'convertHandler.getNum("123.5lbs") should return 123.5'
+      );
+    });
+    test("convertHandler should correctly read a fractional input.", () => {
+      assert.strictEqual(
+        convertHandler.getNum("1/2lbs"),
+        0.5,
+        'convertHandler.getNum("1/2lbs") should return 0.5'
+      );
+    });
+    test("convertHandler should correctly read a fractional input with a decimal.", () => {
+      assert.strictEqual(
+        convertHandler.getNum("1/2.5lbs"),
+        0.4,
+        'convertHandler.getNum("1/2.5lbs") should return 0.4'
+      );
+    });
+    test("convertHandler should correctly return an error on a double-fraction (i.e. 3/2/3).", () => {
+      assert.strictEqual(
+        convertHandler.getNum("3/2/3lbs"),
+        "invalid input",
+        'convertHandler.getNum("3/2/3lbs") should return "invalid input"'
+      );
+    });
+    test("convertHandler should correctly default to a numerical input of 1 when no numerical input is provided.", () => {
+      assert.strictEqual(
+        convertHandler.getNum("lbs"),
+        1,
+        "convertHandler.getNum() should return 1"
+      );
+    });
+  });
 });
